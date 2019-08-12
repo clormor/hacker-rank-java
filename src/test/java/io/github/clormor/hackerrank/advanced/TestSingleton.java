36e6b51b6526a41ee15bf8ea8971a22f43805dcc
Chris Lormor <clormor@palantir.com>
2019-08-12 21:17:00 +0100

    adding singleton solution

--- src/test/java/io/github/clormor/hackerrank/advanced/TestSingleton.java
+++ src/test/java/io/github/clormor/hackerrank/advanced/TestSingleton.java
@@ -1,4 +1,28 @@
 package io.github.clormor.hackerrank.advanced;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class TestSingleton {
+
+    private Singleton s;
+
+    @Before
+    public void setup() {
+        s = Singleton.getSingleInstance();
+    }
+
+    @Test
+    public void simple_test() {
+        s.setStr("hello");
+        assertEquals("hello", s.getStr());
+
+        Singleton s2 = Singleton.getSingleInstance();
+        s.setStr("world");
+
+        assertEquals("world", s2.getStr());
+        assertEquals("world", s.getStr());
+    }
 }
