274496414acadda731192ffc154d03e8484ea0fd
Chris Lormor <clormor@palantir.com>
2019-08-13 11:30:16 +0100

    adding solution to second reflection problem

--- src/main/java/io/github/clormor/hackerrank/advanced/ReflectionExample.java
+++ src/main/java/io/github/clormor/hackerrank/advanced/ReflectionExample.java
@@ -15,11 +15,29 @@ public class ReflectionExample {
 
         Arrays.sort(methodNames);
         StringBuilder result = new StringBuilder();
-        for (String name: methodNames) {
+        for (String name : methodNames) {
             result.append(name).append(System.lineSeparator());
         }
 
         // if building via gradle we might get some jacoco classes, so remove these here
         return result.toString().replaceAll("\\$jacocoInit\\n", "");
+    }
+
+    static class Inner {
+
+        class Private {
+
+            private boolean powerOf2(int n) {
+                if (n == 1) {
+                    return true;
+                }
+                do {
+                    if (n == 2) {
+                        return true;
+                    }
+                } while ((n /= 2) > 1);
+                return false;
+            }
+        }
     }
 }
